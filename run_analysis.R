50fcc66936f94d18f24298f47aaf312a5891d601
davegiff <69947055+davegiff@users.noreply.github.com>
2020-09-18 10:21:26 +1000

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -22,17 +22,19 @@ var_names <- read.table("./UCI HAR Dataset/features.txt")
 # load activity labels
 act_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")
 
-# determine which variables are means and standard deviations and 
-# discards others
+# determine which variables are means and standard deviations and discard others
+
 means<-filter(var_names,str_detect(V2,"mean\\(\\)"))
 stds<-filter(var_names,str_detect(V2,"std\\(\\)"))
 both<-rbind(means,stds)
 keep<-X_data[,both$V1]
+
+# combine subject, activity and observation data and add variable names 
 all_data <-cbind(Sub_data,Y_data,keep)
 colnames(all_data) <- c("Subject","Activity",var_names[both[,1],2])
 
-# remove brackets and hyphens from names, add hyphen back at the start
-# and capitalise
+# remove brackets and hyphens from names, add hyphen back at the start and capitalise
+
 colnames(all_data) <- gsub("\\()","",colnames(all_data))
 colnames(all_data) <- gsub("-","",colnames(all_data))
 colnames(all_data) <- gsub("mean","-Mean",colnames(all_data))
